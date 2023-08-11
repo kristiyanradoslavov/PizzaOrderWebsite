df01080eb40229a7a07c66b4754655cf52c5136c
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-08-12 00:35:04 +0300

    commit new changes

--- PizzaProject/order/admin.py
+++ PizzaProject/order/admin.py
@@ -3,26 +3,10 @@ from django.contrib import admin
 from PizzaProject.order.models import OrderItem, OrderHistory
 
 
-@admin.register(OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ['id', 'product_name', 'size', 'quantity', 'single_price', 'user']
-    search_fields = ['product_name', 'size']
+class OrderBaseAdmin(admin.ModelAdmin):
     search_help_text = 'Search by Name of the product or Size.'
-    list_filter = ['user']
-    list_display_links = ['id', 'product_name']
-
-
-@admin.register(OrderHistory)
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ['id', 'product_name', 'size', 'quantity', 'single_price', 'user', 'specific_order_id',
-                    'date_created']
-    list_filter = ['date_created', 'user']
     search_fields = ['product_name', 'size']
-    search_help_text = 'Search by Name of the product or Size.'
-    actions = ['completed']
     list_display_links = ['id', 'product_name']
-    date_hierarchy = 'date_created'
-
     fieldsets = (
         ('User Information', {
             'fields': ('user',)
@@ -31,3 +15,17 @@ class OrderItemAdmin(admin.ModelAdmin):
             'fields': ('product_name', 'product_id', 'quantity', 'size', 'single_price', 'image', 'price_id')
         }),
     )
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(OrderBaseAdmin):
+    list_display = ['id', 'product_name', 'size', 'quantity', 'single_price', 'user']
+    list_filter = ['user']
+
+
+@admin.register(OrderHistory)
+class OrderHistoryAdmin(OrderBaseAdmin):
+    list_display = ['id', 'product_name', 'size', 'quantity', 'single_price', 'user', 'specific_order_id',
+                    'date_created']
+    list_filter = ['date_created', 'user']
+    date_hierarchy = 'date_created'
--- PizzaProject/products/admin.py
+++ PizzaProject/products/admin.py
@@ -3,21 +3,71 @@ from django.contrib import admin
 from PizzaProject.products.models import Pizza, Salad, Desert, Drink
 
 
+class BaseProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug', ]
+    search_fields = ['name', ]
+    list_display_links = ['id', 'name']
+    search_help_text = 'Search by Name of the product.'
+
+
 @admin.register(Pizza)
-class PizzaAdmin(admin.ModelAdmin):
-    pass
+class PizzaAdmin(BaseProductAdmin):
+    fieldsets = (
+        ('Product information', {
+            'fields': ('name', 'description', 'image')
+        }),
+        ('Price Information', {
+            'fields': ('price_small', 'price_medium', 'price_large', 'price_extra_large')
+        }),
+        ('Stripe prices', {
+            'fields': (
+                'stripe_small_price_id', 'stripe_medium_price_id', 'stripe_large_price_id',
+                'stripe_extra_large_price_id'
+            )
+        })
+    )
 
 
 @admin.register(Salad)
-class SaladAdmin(admin.ModelAdmin):
-    pass
+class SaladAdmin(BaseProductAdmin):
+    fieldsets = (
+        ('Product information', {
+            'fields': ('name', 'description', 'image')
+        }),
+        ('Price Information', {
+            'fields': ('price_small', 'price_large')
+        }),
+        ('Stripe prices', {
+            'fields': ('stripe_small_price_id', 'stripe_large_price_id')
+        })
+    )
 
 
 @admin.register(Desert)
-class SaladAdmin(admin.ModelAdmin):
-    pass
+class DesertAdmin(BaseProductAdmin):
+    fieldsets = (
+        ('Product information', {
+            'fields': ('name', 'description', 'image')
+        }),
+        ('Price Information', {
+            'fields': ('price',)
+        }),
+        ('Stripe prices', {
+            'fields': ('stripe_single_price_id',)
+        })
+    )
 
 
 @admin.register(Drink)
-class SaladAdmin(admin.ModelAdmin):
-    pass
+class DrinkAdmin(BaseProductAdmin):
+    fieldsets = (
+        ('Product information', {
+            'fields': ('name', 'description', 'small_image', 'large_image')
+        }),
+        ('Price Information', {
+            'fields': ('price_small', 'price_large')
+        }),
+        ('Stripe prices', {
+            'fields': ('stripe_small_price_id', 'stripe_large_price_id')
+        })
+    )
